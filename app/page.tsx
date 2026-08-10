0f56e420abf82507c1758d97839c7276c94cc3be
ifrah-ashraf <ifrahashraf48@gmail.com>
2026-08-10 20:25:16 +0530

    initialise the next js repo

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center text-teal-800" >
+      Hello world
+      </h1> 
+      <div className="mt-12 flex justify-center">
+      This is the fresh moment i have been waiting for quite a long time.<br />
+      I will build my logger website from scratch.
+      </div>
+      
+    </div>
+  );
+}
